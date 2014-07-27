1e4ebf8aa03d73d1a9ee718528dd631047da7e7d
magzhan.karasayev <fallen.s4e@gmail.com>
2014-07-28 00:02:52 +0600

    f-converters: Upd: VaadinDateEditor implemented

--- src/main/java/uk/co/innoforce/MyVaadinUI.java
+++ src/main/java/uk/co/innoforce/MyVaadinUI.java
@@ -10,7 +10,7 @@ import com.vaadin.ui.UI;
 import com.vaadin.ui.VerticalLayout;
 import uk.co.innoforce.component.IComponent;
 import uk.co.innoforce.component.IVaadinComponent;
-import uk.co.innoforce.component.editors.VaadinDateTimeEditor;
+import uk.co.innoforce.component.editors.VaadinDateEditor;
 import uk.co.innoforce.component.editors.VaadinStringEditor;
 
 import javax.servlet.annotation.WebServlet;
@@ -44,7 +44,7 @@ public class MyVaadinUI extends UI
         layout.addComponent(new VaadinStringEditor());
 
         //---------------------------------------------------------------------------
-        final IVaadinComponent tf = new VaadinDateTimeEditor();
+        final IVaadinComponent tf = new VaadinDateEditor();
 
         // And bind the field
         layout.addComponent(tf);
