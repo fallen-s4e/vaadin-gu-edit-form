6cd0de02f3934026c090eed057d58c908d9ad1cc
magzhan.karasayev <fallen.s4e@gmail.com>
2014-07-27 18:32:32 +0600

    f-converters: Ref: VaadinAbstractEditor class extracted

--- src/main/java/uk/co/innoforce/component/editors/VaadinBigDecimalEditor.java
+++ src/main/java/uk/co/innoforce/component/editors/VaadinBigDecimalEditor.java
@@ -1,39 +1,15 @@
 package uk.co.innoforce.component.editors;
 
 import com.vaadin.data.util.converter.StringToBigDecimalConverter;
-import com.vaadin.ui.TextField;
-import uk.co.innoforce.component.IVaadinComponent;
 
 import java.math.BigDecimal;
-import java.util.Locale;
 
 /**
  * @author magzhan.karasayev
  * @since 7/25/14 6:30 PM
  */
-public class VaadinBigDecimalEditor extends TextField implements IVaadinComponent<BigDecimal> {
-
+public class VaadinBigDecimalEditor extends VaadinAbstractEditor<BigDecimal> {
     public VaadinBigDecimalEditor() {
-        setConverter(new StringToBigDecimalConverter(){
-            @Override
-            public String convertToPresentation(BigDecimal value, Class<? extends String> targetType, Locale locale) throws ConversionException {
-                return value == null ? "" : super.convertToPresentation(value, targetType, locale);
-            }
-        });
-        setConversionError("Could not convert value to {0}");
-    }
-
-    @Override
-    public BigDecimal getV() throws MalformedInputException {
-        try {
-            return (BigDecimal) getConvertedValue();
-        } catch (Exception e) {
-            throw new MalformedInputException(e);
-        }
-    }
-
-    @Override
-    public void setV(BigDecimal bigDecimal) {
-        setConvertedValue(bigDecimal);
+        super(new StringToBigDecimalConverter());
     }
 }
