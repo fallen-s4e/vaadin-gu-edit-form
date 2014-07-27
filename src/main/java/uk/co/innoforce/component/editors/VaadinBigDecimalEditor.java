d184834b4374aa4ab281d043cf4db9927238129b
magzhan.karasayev <fallen.s4e@gmail.com>
2014-07-27 18:02:04 +0600

    f-converters: Upd: convertion to null fixed

--- src/main/java/uk/co/innoforce/component/editors/VaadinBigDecimalEditor.java
+++ src/main/java/uk/co/innoforce/component/editors/VaadinBigDecimalEditor.java
@@ -6,6 +6,7 @@ import com.vaadin.ui.TextField;
 import uk.co.innoforce.component.IComponent;
 
 import java.math.BigDecimal;
+import java.util.Locale;
 
 /**
  * @author magzhan.karasayev
@@ -17,7 +18,12 @@ public class VaadinBigDecimalEditor extends TextField implements IComponent<BigD
         final ObjectProperty<BigDecimal> property =
                 new ObjectProperty<BigDecimal>(new BigDecimal(42));
 
-        setConverter(new StringToBigDecimalConverter());
+        setConverter(new StringToBigDecimalConverter(){
+            @Override
+            public String convertToPresentation(BigDecimal value, Class<? extends String> targetType, Locale locale) throws ConversionException {
+                return value == null ? "" : super.convertToPresentation(value, targetType, locale);
+            }
+        });
         setConversionError("Could not convert value to {0}");
 
         setPropertyDataSource(property);
