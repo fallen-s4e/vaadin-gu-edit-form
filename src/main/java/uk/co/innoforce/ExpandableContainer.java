bdd2b1072b9a0d89e73fc32e91290e6bcffc42ab
magzhan.karasayev <fallen.s4e@gmail.com>
2014-07-30 13:13:23 +0600

    f-converters: Upd: made label always visible, bold and centered

--- src/main/java/uk/co/innoforce/ExpandableContainer.java
+++ src/main/java/uk/co/innoforce/ExpandableContainer.java
@@ -1,5 +1,6 @@
 package uk.co.innoforce;
 
+import com.vaadin.shared.ui.label.ContentMode;
 import com.vaadin.ui.*;
 
 /**
@@ -11,10 +12,9 @@ public class ExpandableContainer extends Panel implements IExpandableContainer {
     private final VerticalLayout contentPane = new VerticalLayout();
     private final Component label;
 
-    public ExpandableContainer(String sectionName) {
-        label = new Label(sectionName) {{
-            setVisible(false);
-        }};
+    public ExpandableContainer(final String sectionName) {
+        // for some reasons setAlignment(CENTER) does not work, so I set it manually
+        label = new Label("<center><b>" + sectionName + "</b></center>", ContentMode.HTML);
         final GridLayout gl = new GridLayout(2,2);
         gl.addComponent(new Button("-") {{
             addClickListener(new ClickListener() {
@@ -23,13 +23,18 @@ public class ExpandableContainer extends Panel implements IExpandableContainer {
                     contentPane.setVisible(!contentPane.isVisible());
 
                     setCaption(contentPane.isVisible() ? "-" : "+");
-                    label.setVisible(!contentPane.isVisible());
                 }
             });
+            setWidth(30, Unit.PIXELS);
         }});
 
         gl.addComponent(label, 1, 0);
         gl.addComponent(contentPane, 1, 1);
+        gl.setWidth(100, Unit.PERCENTAGE);
+
+        gl.setColumnExpandRatio(0, 0);
+        gl.setColumnExpandRatio(1, 1);
+//        gl.setComponentAlignment(label, Alignment.MIDDLE_CENTER);
 
         setContent(gl);
     }
